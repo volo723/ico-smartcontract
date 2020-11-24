44b27c7486eacbddc7c47f8536747a39efade80d
samuel20011 <guofupang@gmail.com>
2020-11-25 05:39:22 +1000

    test case

--- ETRToken_Test.sol
+++ ETRToken_Test.sol
@@ -1,45 +1,58 @@
 // Test Case
 
-//////////////////////////////// Deployment //////////////////////////////////
-// 1. deploy with contract address as owner or team address.
+//////////////////////////////// 1. Deployment //////////////////////////////////
+// 1.1. deploy with contract address as owner or team address.
 
-// 2. deploy using the same team address as the owner's address.
+// 1.2. deploy using the same team address as the owner's address.
 
-// 3. if contact is deployed successfully, then check if the team and owner received 50% of total supply.
+// 1.3. if contact is deployed successfully, then check if the team and owner received 50% of total supply.
 
-//////////////////////////////// Configuration //////////////////////////////////
-// 4. Invest Ether without setting startDate, endDate
+//////////////////////////////// 2. Configuration //////////////////////////////////
+// 2.1. Invest Ether without setting startDate, endDate, or startDate > endDate
+//    - will return error message "The configuration was not set yet."
+
+// 2.2. after the ICO started, set startDate or endDate
+//    - will return error message "The ICO already has been executed. Cannot update anymore.".
+
+// 2.3. if non owner try to set it, then it should be error.
+//
+
+//////////////////////////////// 3. Investement //////////////////////////////////
+
+// 3.1. set startDate, set endDate, and invest less than INVEST_MIN_AMOUNT = 0.01 ether
 //    - will return error message.
 
-// 5. set startDate using setStartDate function, and invest
-//    - will return error message because of endDate.
-
-// 6. No set startDate, set endDate using setEndDate function, and invest
-//    - will return error message because of startDate.
-
-// 7. No set startDate, set endDate using setEndDate function, and invest
-//    - will return error message because of startDate.
-
-//////////////////////////////// Investement //////////////////////////////////
-
-// 8. set startDate, set endDate, and invest less than INVEST_MIN_AMOUNT = 0.01 ether
-//    - will return error message.
-
-// 9. invest in smart contract.
+// 3.2. invest in smart contract.
 //    - will return error message "Cannot be a contract".
 
-// 10. Contract owner invests.
+// 3.3. Contract owner invests.
 //    - will return error message "You are onwer."
 
-// 11. Invest more than minimum ether limit by week (1,2,3,4, ...)
+// 3.4. Invest more than minimum ether limit by week (1,2,3,4, ...)
 //    - will receive ETR token instead of ether. (300%, 200%, 150%, 125%, 100%); 1 ether = 200,000 etr
 
-//////////////////////////////// Completed ICO //////////////////////////////////
-// 12. if ICO completed successfully, then owner can withraw the invested ether.
+// 3.5 Before start date, invest.
+//    - It should return the error message "ICO is not started yet."
+
+
+//////////////////////////////// 4. Completed ICO //////////////////////////////////
+// 4.1. if ICO completed successfully, then owner can withraw the invested ether.
 //     if not, he cannot withraw it.
 
-// 13. if invested ether is less than soft cap, then the ICO will be failed.
+// 4.2. if invested ether is less than soft cap, then the ICO will be failed.
 //     then owner can refund all ethers.
 
-// 14. if invested ether is less than soft cap, then the ICO will be failed.
+// 4.3. if invested ether is less than soft cap, then the ICO will be failed.
 //      Then users can withdraw their ether using refund function.
+
+//////////////////////////////// 5. Owner //////////////////////////////////
+// 5.1. Owner can get user information with user id
+//      getUserInformation()
+
+//////////////////////////////// 6. Users //////////////////////////////////
+// 6.1.User can get Referral number
+//      getMyReferralNumber()
+
+// 6.1.User can get his information
+//      getMyInformation()
+    
